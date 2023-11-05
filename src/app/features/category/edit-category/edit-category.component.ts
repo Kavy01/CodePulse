4fb13168b8ff0646e71a3b3d886573f1dd2fc880
Kavyansh Singh <81253296+Kavy01@users.noreply.github.com>
2023-11-06 00:21:30 +0530

    Add files via upload

---
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styleUrls: ['./edit-category.component.css']
+})
+export class EditCategoryComponent implements OnInit{
+
+  id: string | null = null;
+  category?: Category;
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService, private router: Router){}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id')
+
+        if(this.id)
+        {
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next: (response) => {
+              this.category = response;
+            }
+          })
+        }
+      }
+    });
+  }
+
+  onFormSubmit(): void{
+    const updateCategoryRequest : UpdateCategoryRequest = {
+      name: this.category?.name ?? '',
+      urlHandle: this.category?.urlHandle ?? ''
+    }
+    if(this.id)
+    {
+      this.categoryService.updateCategory(this.id, updateCategoryRequest)
+      .subscribe({
+        next: (respoonse) => {
+          this.router.navigateByUrl('/admin/categories');
+        }
+      });
+    }
+  }
+
+  onDelete(): void{
+    if(this.id)
+    {
+      this.categoryService.deleteCategory(this.id).subscribe
+      ({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/categories');
+        }
+      });
+    }
+    
+  }
+}
