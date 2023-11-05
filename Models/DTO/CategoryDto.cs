93e108d4b940d0c4b68705e9a9359aa4a958c1f9
Kavyansh Singh <81253296+Kavy01@users.noreply.github.com>
2023-11-06 00:14:41 +0530

    Delete Models directory

--- Models/DTO/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp.Models.DTO
-{
-    public class CategoryDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string UrlHandle { get; set; }
-    }
-}
--- Models/DTO/CreateCategoryRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp.Models.DTO
-{
-    public class CreateCategoryRequestDto
-    {
-        public string Name { get; set; }
-        public string UrlHandle { get; set; }
-    }
-}
